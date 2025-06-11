5fc47e3e0aa0328146e01ce16adef8bc60a081ab
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-06-11 11:30:33 -0400

    feat(TodoCounter): Add TodoCounter class and integrate with todo create, complete and delete logic

--- pages/index.js
+++ pages/index.js
@@ -3,44 +3,88 @@ import { Todo } from "../components/todo.js";
 import { FormValidator } from "../components/formValidator.js";
 import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
+import TodoCounter from "../components/TodoCounter.js";
 
+//DOM Elements
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
-const formValidator = new FormValidator(validationConfig, addTodoForm);
 
+//Enable form validation
+const formValidator = new FormValidator(validationConfig, addTodoForm);
 formValidator.enableValidation();
 
+//Add new todos upon form submission
 const popupForm = new PopupWithForm({
   popupSelector: "#add-todo-popup",
-  handleFormSubmit: (formValues) => {
-    //   // Create a date object and adjust for timezone
-    formValues.date = new Date(formValues.date);
-    formValues.date.setMinutes(
-      formValues.date.getMinutes() + formValues.date.getTimezoneOffset()
+  handleFormSubmit: (formInputValues) => {
+    //Using data captured within the PopupWithForm class  // Create a date object and adjust for timezone
+    formInputValues.date = new Date(formInputValues.date);
+    formInputValues.date.setMinutes(
+      formInputValues.date.getMinutes() +
+        formInputValues.date.getTimezoneOffset()
     );
-
-    const todo = new Todo(formValues, "#todo-template");
+    //Pass formatted form inputs to create a new instance of the Todo class
+    const todo = new Todo(formInputValues, "#todo-template", {
+      //Use two seperate callbacks in the Todo class to handle the todo counter
+      handleTodoDelete: (wasCompleted) => {
+        todoCounter.updateTotal(false);
+        if (wasCompleted) {
+          todoCounter.updateCompleted(false);
+        }
+      },
+      handleTodoComplete: (isCompleted) => {
+        isCompleted
+          ? todoCounter.updateCompleted(true)
+          : todoCounter.updateCompleted(false);
+      },
+    });
+    //Create a DOM element from the new todo instance using todo's getView() method and
+    // render it(add it to the DOM) using an instance of the Section class.
     todoListSection.addItem(todo.getView());
+    //Reset the form after submission.
     formValidator.resetValidation();
+    //Close the form after submission using a method inherited from the Popup class.
     popupForm.close();
+    //Update the total todos when new todos are created
+    todoCounter.updateTotal(true);
   },
 });
 
+//Sets the submission event listener and closing logic listeners on the form and popup
 popupForm.setEventListeners();
 
+//Sets the eventListener to open the popup
 addTodoButton.addEventListener("click", () => {
   popupForm.open();
 });
 
+// Create and render todos from intialTodos array. Establishes the instance of
+// the Section class used to render intital todos and new todos.
 const todoListSection = new Section({
   items: initialTodos,
   renderer: (item) => {
-    const todo = new Todo(item, "#todo-template");
-    const todoElement = todo.getView();
-    todoListSection.addItem(todoElement);
+    const todo = new Todo(item, "#todo-template", {
+      handleTodoDelete: (wasCompleted) => {
+        todoCounter.updateTotal(false);
+        if (wasCompleted) {
+          todoCounter.updateCompleted(false);
+        }
+      },
+      handleTodoComplete: (isCompleted) => {
+        isCompleted
+          ? todoCounter.updateCompleted(true)
+          : todoCounter.updateCompleted(false);
+      },
+    });
+
+    todoListSection.addItem(todo.getView());
   },
   containerSelector: ".todos__list",
 });
 
+//Applies the renderer callback function to each data item.
 todoListSection.renderItems();
+
+//Creates a new instance of the TodoCounter so methods like updateTotal() or updateCompleted() are available.
+const todoCounter = new TodoCounter(initialTodos, ".counter__text");
